365cb72bf625ba670acd77e9d0515cf8c72a85d2
vainawebcpx <vainawebcpx@gmail.com>
2017-11-24 17:17:45 -0200

    ImpactoGerado

---
+++ impactoge/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header">
+    <h1 class="headerTitle">Impacto gerado</h1>
+    <p  class="headerParagraph">O ReciclAção propõe estimular e mobilizar a comunidade a descartar adequadamente seus materiais recicláveis, retirando-os do meio ambiente e dando origem a um ciclo fértil e produtivo que retorna em forma de recursos locais, com ênfase na educação e na cooperação
+    <p  class="headerParagraph">O Projeto reciclação começou 3 anos após o desabamento do campinho,
+    na Rua Gomes Lopes em 2010, provocado pelo temporal e acúmulo de lixo nas encostas.36 pessoas
+    morreram e 7 mil ficaram desabrigadas. A partir disso o grupo PROA, junto com a Cris dos razeres e sua equipe de trabalho, criaram o projeto ReciclAção para promover ações de conservação e conscientização ambiental no morro dos Prazeres, concentradas no bem-estar das pessoas através do combate de doenças como dengue, zica e chikungunya e outras doenças causadas por acúmulo de lixo.</p>
+  <div class="grafico">
+    <h1 class="graficoTitle">2012</h1>
+
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.header {
+  background: #333333;
+  text-align: left;
+  padding: 50px; 
+  /*
+  display: flex;
+  flex-direction: column;*/
+
+}
+
+.headerTitle {
+  color: #85c226;
+  font-size: 2.5em;
+
+}
+
+.headerParagraph{
+  color: #fff;
+  font-size: 1.25;
+}
+
+.grafico{
+
+}
+
+.graficoTitle{
+  color: #85c226;
+  font-size: 2.75em;
+}
+
+</style>
